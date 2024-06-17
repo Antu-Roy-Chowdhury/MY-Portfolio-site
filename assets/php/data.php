30b86ee2a6a6892701e45d6b67177b362c62961d
Antu Roy Chowdhury <anturoychowdhury5@gmail.com>
2024-06-18 01:45:09 +0600

    modified:   assets/php/data.php

--- assets/php/data.php
+++ assets/php/data.php
@@ -6,4 +6,22 @@ if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $message = $_POST['message'];
 }
+
+// Database connection
+
+$servername = "localhost";
+$username = "root";
+$password ="";
+$dbname = "portfolio_response";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into visitor_reponse(Name, Email, Messages) values(?, ?, ?)");
+    $stmt->bind_param("sss", $name, $email, $message);
+    $stmt->execute();
+    echo "Message Sent Successfully...";
+    $stmt->close();
+    $conn->close();
+}
 ?>
